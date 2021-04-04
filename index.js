393498058f3e0b0f039a68b3ac6552bab7d0de6a
sydney <sydneychase5841@gmail.com>
2021-04-04 14:24:51 -0400

    Saving Assignment Moving on

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(num) {
+  let ride
+  if (num <= 400) {
+  ride = 199
+  return ('This one is on me!')
+  }
+  else if (num > 2000) {
+    ride = 2001
+    return ('I will gladly take your thirty bucks.')
+  
+  }
+  else if (num > 2500) {
+    ride = 2501
+    return ('No can do.')
+  }
 }
+ 
+
 
 function ternaryCheckCity(){
   // Write your code here!
